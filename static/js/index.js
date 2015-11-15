6ca5236c7c25a4c3f5dc3f6b17ef831ee30abb2b
Ben Bales <bbbales2@gmail.com>
2015-11-15 12:43:50 -0800

    Added a bunch of UI stuff

---
+++ static/js/index.js
@@ -0,0 +1,109 @@
+var Index = Index || {}
+
+Index.Controller = Backbone.View.extend(
+    {
+        events : {
+        },
+
+        initialize : function(options)
+        {
+        },
+
+        drawCards : function()
+        {
+            var template = _.template($( '.cardTemplate' ).text());
+
+            $( '.handCards' ).empty();
+            $( '.holeCards' ).empty();
+
+            $( '.bets' ).text( this.data.bets );
+
+            var rankToString = { 2 : '2',
+                                 3 : '3',
+                                 4 : '4',
+                                 5 : '5',
+                                 6 : '6',
+                                 7 : '7',
+                                 8 : '8',
+                                 9 : '9',
+                                 T : '10',
+                                 J : 'jack',
+                                 Q : 'queen',
+                                 K : 'king',
+                                 A : 'ace' };
+            
+            var suitToString = { s : 'spades',
+                                 d : 'diamonds',
+                                 c : 'clubs',
+                                 h : 'hearts' };
+
+            for(var i = 0; i < this.data.hcards.length; i++)
+            {
+                var card = this.data.hcards[i];
+
+                var url = 'static/images/';
+
+                url += rankToString[card[0]] + '_of_' + suitToString[card[1]] + '.png';
+
+                $( template({ url : url }) ).appendTo( $( '.handCards' ) );
+            }
+
+            for(var i = 0; i < this.data.cards.length; i++)
+            {
+                var card = this.data.cards[i];
+
+                var url = 'static/images/';
+
+                url += rankToString[card[0]] + '_of_' + suitToString[card[1]] + '.png';
+
+                $( template({ url : url }) ).appendTo( $( '.holeCards' ) );
+            }
+        },
+        
+        betFailed : function()
+        {
+            this.$el.find( '.msg' ).text('Bet failed, try again later');
+        },
+
+        do_action : function(play)
+        {
+            $.ajax( {
+                url : '/play',
+                data : { action : play },
+                dataType : 'json',
+                method : 'GET',
+                success : _.bind(this.action_result, this)
+            } );
+        },
+
+        action_result : function(data)
+        {
+            this.data = data.state;
+
+            this.drawCards();
+        },
+
+        render : function()
+        {
+            this.$el = $("body");
+            this.el = this.$el[0];
+
+            this.data = $.parseJSON(this.$el.find( '.data' ).text());
+
+            $( '.raise' ).click(_.bind(_.partial(this.do_action, 'r'), this));
+            $( '.check' ).click(_.bind(_.partial(this.do_action, 'c'), this));
+            $( '.fold' ).click(_.bind(_.partial(this.do_action, 'f'), this));
+
+            this.drawCards();
+
+            this.delegateEvents();
+        },
+    }
+);
+
+$( document ).ready( function() {
+    var cont = new Index.Controller();
+    
+    cont.render();
+});
+
--- webui.py
+++ webui.py
@@ -2,56 +2,149 @@ import flask
 import os
 import sys
 import socket
+import random
+import flask.ext.session
+import json
 
-bufferSize = 1024
+def make_decision(player, roundNumber, bets, cards):
+    raises = 0
+
+    bets = bets.split('/')[-1]
+
+    if len(bets) > 0:
+        if bets[0] == 'r':
+            for b in bets[1:]:
+                if b == 'r':
+                    raises += 1
+        else:
+            for b in bets[2:]:
+                if b == 'r':
+                    raises += 1
+    
+    goesfirst = (player == 1 and roundNumber == 0) or (player == 0 and roundNumber > 0)
+
+    print raises, bets
+
+    if (len(bets) == 0 and goesfirst) or (not goesfirst and len(bets) == 1 and bets[0] == 'c'):
+        choices = ['c', 'r']
+    elif raises == 3:
+        choices = ['c', 'f']
+    else:
+        choices = ['c', 'r', 'f']
+
+    return random.choice(choices)
 
 appDir = os.path.abspath(os.path.dirname(__file__))
 
 app = flask.Flask('poker', template_folder = appDir + '/templates/')
 
+session = flask.ext.session.Session()
+
 states = []
 actions = []
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(1.0)
-s.connect(('127.0.0.1', int(sys.argv[1])))
-s.send('VERSION:2:0:0\r\n')
-#s.close()
-print "listening to socket!"
+def get_state():
+    return { 'round' : flask.session['round'],
+             'bets' : flask.session['bets'],
+             'cards' : flask.session['cards'],
+             'hcards' : flask.session['hcards'] }
+
+def computer_play():
+    pass
+
+def make_play(player, action):
+    if flask.session['winner'] > -1:
+        return
+
+    flask.session['bets'] += action
+
+    if action == 'f':
+        flask.session['winner'] = 1 - player
+        return 'next'
+
+    raises = 0
+
+    bets = flask.session['bets'].split('/')[-1]
+
+    if len(bets) > 1 and action == 'c':
+        if flask.session['round'] == 0:
+            flask.session['cards'].append(flask.session['deck'].pop())
+            flask.session['cards'].append(flask.session['deck'].pop())
+            flask.session['cards'].append(flask.session['deck'].pop())
+        elif flask.session['round'] == 1:
+            flask.session['cards'].append(flask.session['deck'].pop())
+        elif flask.session['round'] == 2:
+            flask.session['cards'].append(flask.session['deck'].pop())
+        elif flask.sesion['round'] == 3:
+            flask.session['winner'] = random.randint(0, 1)
+
+        flask.session['round'] += 1
+        flask.session['bets'] += '/'
+
+        return 'next'
+
+    return None
 
 @app.route('/play')
 def play():
     action = flask.request.args.get('action', None)
 
-    if action in 'fcr':
-        message = "{0}:{1}".format(states[-1] if len(states) > 0 else '', action)
-        print "web ui writes: " + message
-        s.send(message)
+    ret = make_play(0, action)
 
-    return flask.json.jsonify({})
+    if not ret:
+        computerAction = make_decision(1, flask.session['round'], flask.session['bets'], flask.session['ccards'] + flask.session['cards'])
 
-@app.route('/get_state')
-def get_state():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.settimeout(1.0)
-        s.connect(('127.0.0.1', int(sys.argv[1])))
-        data = s.recv(bufferSize)
-        
-        if len(data) > 0:
-            print "web ui receives: " + data
-            states.append(data)
-    except socket.timeout:
-        print "Socket timeout"
-    finally:
-        s.close()
-
-    return flask.json.jsonify(state = states[-1] if len(states) > 0 else '')
+        make_play(1, computerAction)
+    
+    return flask.json.jsonify(state = get_state())
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    reset = flask.request.args.get('reset', None)
+
+    if (reset is not None or
+        'round' not in flask.session or
+        'bets' not in flask.session or
+        'cards' not in flask.session or
+        'hcards' not in flask.session or
+        'ccards' not in flask.session):
+        flask.session['round'] = 0
+        flask.session['bets'] = ''
+        flask.session['cards'] = []
+        flask.session['winner'] = -1
+
+        ranks = '23456789TJQKA'
+        suits = 'shdc'
+
+        deck = []
+        for r in ranks:
+            for s in suits:
+                deck.append(r + s)
+
+        random.shuffle(deck)
+
+        p0 = []
+        p1 = []
+
+        p0.append(deck.pop())
+        p1.append(deck.pop())
+        p0.append(deck.pop())
+        p1.append(deck.pop())
+
+        flask.session['hcards'] = p0
+        flask.session['ccards'] = p1
+
+        computerAction = make_decision(1, flask.session['round'], flask.session['bets'], flask.session['ccards'] + flask.session['cards'])
+
+        make_play(1, computerAction)
+
+        flask.session['deck'] = deck
+        
+    return flask.render_template('index.html', data = json.dumps(get_state()))
 
 if __name__ == '__main__':
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    session.init_app(app)
     app.run(debug = True)
 
